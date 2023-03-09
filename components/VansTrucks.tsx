e91453bd0d9a539411cede0ba3e4018dd3ff064b
Rajpreet0 <rajpreet.singh@gmx.de>
2023-03-09 14:22:51 +0100

    mobile right iframe

--- components/VansTrucks.tsx
+++ components/VansTrucks.tsx
@@ -10,12 +10,12 @@ const VansTrucks: React.FC = () => {
         <p className='text-center text-[#c6c6c6] mt-2'>Unsere ganzen Angebote von unseren Truck und unseren Vans werden bei uns über Mobile.de verhandelt und beworben</p>
         <div className='p-[5%]'>
           <iframe 
-          src="https://home.mobile.de/home/index.html?partnerHead=false&colorTheme=default&customerId=5562105#ses" 
+          src="https://home.mobile.de/home/index.html?partnerHead=false&amp;colorTheme=default&amp;customerId=11719026#ses" 
           name='Fahrzeugangebot' id="Fahrzeugangebot"
           className='w-full h-screen border-2 rounded-lg'/>
           <a 
           target="_blank" rel='noopener noreferrer'
-          href='https://suchen.mobile.de/fahrzeuge/details.html?id=361542077&damageUnrepaired=ALSO_DAMAGE_UNREPAIRED&isSearchRequest=true&pageNumber=1&scopeId=MH&action=topOfPage&top=1:1&searchId=6d71593e-9e3e-9ade-e428-8c2d9cc464eb&ref=srp'>
+          href='https://home.mobile.de/home/index.html?partnerHead=false&amp;colorTheme=default&amp;customerId=11719026#ses'>
           <div className='w-[250px] h-[55px] bg-[#ff8635] mt-[25px] shadow-lg rounded-lg flex cursor-pointer hover:scale-105 transition-all'>
              <div className='flex flex-rowx items-center justify-around w-full'>
                 <Image src={MobileLogo} alt='' width={70} height={70}/>
@@ -30,3 +30,14 @@ const VansTrucks: React.FC = () => {
 }
 
 export default VansTrucks
+
+
+
+/*
+
+Tutorial zur Anbindung von iFrames über Mobile De
+
+https://wordpress-mobile.de/fahrzeuge-per-iframe-einbinden/
+
+
+*/
