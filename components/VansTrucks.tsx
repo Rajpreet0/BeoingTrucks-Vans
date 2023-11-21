17c74abdcc6a22bf518dd17fff247756d8fe6fb9
Rajpreet0 <rajpreet.singh@gmx.de>
2023-11-21 10:29:46 +0100

    update mobile.de account

--- components/VansTrucks.tsx
+++ components/VansTrucks.tsx
@@ -10,12 +10,12 @@ const VansTrucks: React.FC = () => {
         <p className='text-center text-[#c6c6c6] mt-2'>Unsere ganzen Angebote von unseren Truck und unseren Vans werden bei uns über Mobile.de verhandelt und beworben</p>
         <div className='p-[5%]'>
           <iframe 
-          src="https://home.mobile.de/home/index.html?partnerHead=false&amp;colorTheme=default&amp;customerId=11719026#ses" 
+          src="https://home.mobile.de/home/index.html?partnerHead=false&amp;colorTheme=default&amp;customerId=33722269#ses" 
           name='Fahrzeugangebot' id="Fahrzeugangebot"
           className='w-full h-screen border-2 rounded-lg'/>
           <a 
           target="_blank" rel='noopener noreferrer'
-          href='https://home.mobile.de/home/index.html?partnerHead=false&amp;colorTheme=default&amp;customerId=11719026#ses'>
+          href='https://home.mobile.de/BOEINGTRUCKSVANSGMBH#ses'>
           <div className='w-[250px] h-[55px] bg-[#ff8635] mt-[25px] shadow-lg rounded-lg flex cursor-pointer hover:scale-105 transition-all'>
              <div className='flex flex-rowx items-center justify-around w-full'>
                 <Image src={MobileLogo} alt='' width={70} height={70}/>
